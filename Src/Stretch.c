4b856be74fd165f22d7259457d14c2c286147123
SinoaggDingLiping <dinglp@sinoagg.com>
2018-09-10 17:15:34 +0800

    stretch

--- Src/Stretch.c
+++ Src/Stretch.c
@@ -15,6 +15,7 @@
 #include "LoadPanel.h" 
 //==============================================================================
 // Constants
+int val; 
 char a[15][20]={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
 //==============================================================================
 // Types
@@ -35,23 +36,66 @@ char a[15][20]={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15
 /// HIPAR x/What inputs does your function expect?
 /// HIRET What does your function return?
 
+int CVICALLBACK ModeCallback (int panel, int control, int event,
+							  void *callbackData, int eventData1, int eventData2)
+{
+	switch (event)
+	{
+		case EVENT_LEFT_CLICK_UP:
+			GetCtrlAttribute (hstretchPanel, STRETCH_STRAINTABLE, ATTR_VISIBLE, &val);
+			if(val)
+			{
+				SetCtrlAttribute (hstretchPanel, STRETCH_STRAINTABLE, ATTR_VISIBLE, 0);
+				SetCtrlAttribute (hstretchPanel, STRETCH_LOADTABLE, ATTR_VISIBLE, 1);
+				SetCtrlVal (hstretchPanel, STRETCH_MODE, "  Strain");
+			}
+			else
+			{
+				SetCtrlAttribute (hstretchPanel, STRETCH_STRAINTABLE, ATTR_VISIBLE, 1);
+				SetCtrlAttribute (hstretchPanel, STRETCH_LOADTABLE, ATTR_VISIBLE, 0);
+				SetCtrlVal (hstretchPanel, STRETCH_MODE, "  Load");
+			}
+			break;
+
+	}
+	return 0;
+}
+
 int CVICALLBACK DeleteCallback (int panel, int control, int event,
 								void *callbackData, int eventData1, int eventData2)
 {
 	switch (event)
 	{
 		case EVENT_LEFT_CLICK_UP:
-			Rect range;
+		    Rect range;
 			int row;
-			GetTableSelection (hstretchPanel, STRETCH_TABLE, &range);
-			if(range.left==1&range.width==8)
-			DeleteTableRows (hstretchPanel, STRETCH_TABLE, range.top, range.height); 
-			GetNumTableRows (hstretchPanel, STRETCH_TABLE, &row);   //获取行数
-			for(int i=1;i<row+1;i++)	  //重新编号
+			GetCtrlAttribute (hstretchPanel, STRETCH_STRAINTABLE, ATTR_VISIBLE, &val);
+			if(val)
 			{
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, i, ATTR_USE_LABEL_TEXT, 1);
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, i, ATTR_LABEL_TEXT, a[i-1]);
+				GetTableSelection (hstretchPanel, STRETCH_STRAINTABLE, &range);
+				if(range.left==1&range.width==6)
+				DeleteTableRows (hstretchPanel, STRETCH_STRAINTABLE, range.top, range.height); 
+				GetNumTableRows (hstretchPanel, STRETCH_STRAINTABLE, &row);   //获取行数
+				for(int i=1;i<row+1;i++)	  //重新编号
+				{
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, i, ATTR_USE_LABEL_TEXT, 1);
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, i, ATTR_LABEL_TEXT, a[i-1]);
+				}
 			}
+			else
+			{
+				GetTableSelection (hstretchPanel, STRETCH_LOADTABLE, &range);
+				if(range.left==1&range.width==6)
+				DeleteTableRows (hstretchPanel, STRETCH_LOADTABLE, range.top, range.height); 
+				GetNumTableRows (hstretchPanel, STRETCH_LOADTABLE, &row);   //获取行数
+				for(int i=1;i<row+1;i++)	  //重新编号
+				{
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, i, ATTR_USE_LABEL_TEXT, 1);
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, i, ATTR_LABEL_TEXT, a[i-1]);
+				}
+				
+			}
+			
 			break;
 	}
 	return 0;
@@ -65,22 +109,43 @@ int CVICALLBACK InsabvCallback (int panel, int control, int event,
 		case EVENT_LEFT_CLICK_UP:
 			Rect range;		  
 			int row;
-			GetTableSelection (hstretchPanel, STRETCH_TABLE, &range);	 //获取当前选中范围
-			if(range.left==1&range.width==8) 
+			GetCtrlAttribute (hstretchPanel, STRETCH_STRAINTABLE, ATTR_VISIBLE, &val);
+			if(val)
 			{
-				InsertTableRows (hstretchPanel, STRETCH_TABLE, range.top, 1, VAL_CELL_STRING);   //插入一行
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, range.top, ATTR_SIZE_MODE, VAL_USE_EXPLICIT_SIZE);  //行高设为可调
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, range.top, ATTR_ROW_HEIGHT, 30);	 //行高设为30
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, range.top, ATTR_LABEL_FONT, VAL_MENU_FONT);   //行标签字体
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, range.top, ATTR_LABEL_POINT_SIZE, 12);		  //行标签字号
-				GetNumTableRows (hstretchPanel, STRETCH_TABLE, &row);   //获取行数
-				for(int i=1;i<row+1;i++)	  //重新编号
+				GetTableSelection (hstretchPanel, STRETCH_STRAINTABLE, &range);	 //获取当前选中范围
+				if(range.left==1&range.width==6) 
 				{
-					SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, i, ATTR_USE_LABEL_TEXT, 1);
-					SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, i, ATTR_LABEL_TEXT, a[i-1]);
+					InsertTableRows (hstretchPanel, STRETCH_STRAINTABLE, range.top, 1, VAL_CELL_STRING);   //插入一行
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, range.top, ATTR_SIZE_MODE, VAL_USE_EXPLICIT_SIZE);  //行高设为可调
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, range.top, ATTR_ROW_HEIGHT, 30);	 //行高设为30
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, range.top, ATTR_LABEL_FONT, VAL_MENU_FONT);   //行标签字体
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, range.top, ATTR_LABEL_POINT_SIZE, 12);		  //行标签字号
+					GetNumTableRows (hstretchPanel, STRETCH_STRAINTABLE, &row);   //获取行数
+					for(int i=1;i<row+1;i++)	  //重新编号
+					{
+						SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, i, ATTR_USE_LABEL_TEXT, 1);
+						SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, i, ATTR_LABEL_TEXT, a[i-1]);
+					}
 				}
 			}
-			
+			else
+			{
+				GetTableSelection (hstretchPanel, STRETCH_LOADTABLE, &range);	 //获取当前选中范围
+				if(range.left==1&range.width==6) 
+				{
+					InsertTableRows (hstretchPanel, STRETCH_LOADTABLE, range.top, 1, VAL_CELL_STRING);   //插入一行
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, range.top, ATTR_SIZE_MODE, VAL_USE_EXPLICIT_SIZE);  //行高设为可调
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, range.top, ATTR_ROW_HEIGHT, 30);	 //行高设为30
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, range.top, ATTR_LABEL_FONT, VAL_MENU_FONT);   //行标签字体
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, range.top, ATTR_LABEL_POINT_SIZE, 12);		  //行标签字号
+					GetNumTableRows (hstretchPanel, STRETCH_LOADTABLE, &row);   //获取行数
+					for(int i=1;i<row+1;i++)	  //重新编号
+					{
+						SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, i, ATTR_USE_LABEL_TEXT, 1);
+						SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, i, ATTR_LABEL_TEXT, a[i-1]);
+					}
+				}
+			}
 			break;
 	}
 	return 0;
@@ -94,24 +159,47 @@ int CVICALLBACK InsblwCallback (int panel, int control, int event,
 		case EVENT_LEFT_CLICK_UP:
 			Rect range;
 			int row;
-			GetTableSelection (hstretchPanel, STRETCH_TABLE, &range);
-			if(range.left==1&range.width==8)
+			GetCtrlAttribute (hstretchPanel, STRETCH_STRAINTABLE, ATTR_VISIBLE, &val);
+			if(val)
 			{
-				InsertTableRows (hstretchPanel, STRETCH_TABLE, range.top+1, 1, VAL_CELL_STRING);
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, range.top+1, ATTR_SIZE_MODE, VAL_USE_EXPLICIT_SIZE); 
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, range.top+1, ATTR_ROW_HEIGHT, 30);
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, range.top+1, ATTR_LABEL_FONT, VAL_MENU_FONT);
-				SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, range.top+1, ATTR_LABEL_POINT_SIZE, 12);
-				GetNumTableRows (hstretchPanel, STRETCH_TABLE, &row);
-				for(int i=1;i<row+1;i++)
+				GetTableSelection (hstretchPanel, STRETCH_STRAINTABLE, &range);
+				if(range.left==1&range.width==6)
 				{
-					SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, i, ATTR_USE_LABEL_TEXT, 1);
-					SetTableRowAttribute (hstretchPanel, STRETCH_TABLE, i, ATTR_LABEL_TEXT, a[i-1]);
+					InsertTableRows (hstretchPanel, STRETCH_STRAINTABLE, range.top+1, 1, VAL_CELL_STRING);
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, range.top+1, ATTR_SIZE_MODE, VAL_USE_EXPLICIT_SIZE); 
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, range.top+1, ATTR_ROW_HEIGHT, 30);
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, range.top+1, ATTR_LABEL_FONT, VAL_MENU_FONT);
+					SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, range.top+1, ATTR_LABEL_POINT_SIZE, 12);
+					GetNumTableRows (hstretchPanel, STRETCH_STRAINTABLE, &row);
+					for(int i=1;i<row+1;i++)
+					{
+						SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, i, ATTR_USE_LABEL_TEXT, 1);
+						SetTableRowAttribute (hstretchPanel, STRETCH_STRAINTABLE, i, ATTR_LABEL_TEXT, a[i-1]);
+					}
 				}
 			}
-			
+			else
+			{
+				GetTableSelection (hstretchPanel, STRETCH_LOADTABLE, &range);
+				if(range.left==1&range.width==6)
+				{
+					InsertTableRows (hstretchPanel, STRETCH_LOADTABLE, range.top+1, 1, VAL_CELL_STRING);
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, range.top+1, ATTR_SIZE_MODE, VAL_USE_EXPLICIT_SIZE); 
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, range.top+1, ATTR_ROW_HEIGHT, 30);
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, range.top+1, ATTR_LABEL_FONT, VAL_MENU_FONT);
+					SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, range.top+1, ATTR_LABEL_POINT_SIZE, 12);
+					GetNumTableRows (hstretchPanel, STRETCH_LOADTABLE, &row);
+					for(int i=1;i<row+1;i++)
+					{
+						SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, i, ATTR_USE_LABEL_TEXT, 1);
+						SetTableRowAttribute (hstretchPanel, STRETCH_LOADTABLE, i, ATTR_LABEL_TEXT, a[i-1]);
+					}
+				}
+			}
 			break;
 
 	}
 	return 0;
 }
+
+
--- UI Files/StretchPanel.h
+++ UI Files/StretchPanel.h
@@ -14,27 +14,29 @@
      /* Panels and Controls: */
 
 #define  STRETCH                          1
-#define  STRETCH_TABLE                    2       /* control type: table, callback function: (none) */
-#define  STRETCH_TEXTMSG_11               3       /* control type: textMsg, callback function: (none) */
-#define  STRETCH_TEXTMSG_12               4       /* control type: textMsg, callback function: (none) */
-#define  STRETCH_TEXTMSG_13               5       /* control type: textMsg, callback function: (none) */
-#define  STRETCH_TEXTMSG_15               6       /* control type: textMsg, callback function: (none) */
-#define  STRETCH_TEXTMSG_10               7       /* control type: textMsg, callback function: (none) */
-#define  STRETCH_TEXTMSG_9                8       /* control type: textMsg, callback function: (none) */
-#define  STRETCH_TEXTMSG_16               9       /* control type: textMsg, callback function: (none) */
-#define  STRETCH_TEXTMSG_8                10      /* control type: textMsg, callback function: (none) */
-#define  STRETCH_TEXTMSG                  11      /* control type: textMsg, callback function: (none) */
-#define  STRETCH_DELETE                   12      /* control type: textMsg, callback function: DeleteCallback */
-#define  STRETCH_INSBLW                   13      /* control type: textMsg, callback function: InsblwCallback */
-#define  STRETCH_PICTURE_5                14      /* control type: picture, callback function: InsabvCallback */
-#define  STRETCH_TEXTMSG_14               15      /* control type: textMsg, callback function: (none) */
-#define  STRETCH_INSABV                   16      /* control type: textMsg, callback function: InsabvCallback */
-#define  STRETCH_NUMERIC_3                17      /* control type: numeric, callback function: (none) */
-#define  STRETCH_NUMERIC_2                18      /* control type: numeric, callback function: (none) */
-#define  STRETCH_PICTURE_6                19      /* control type: picture, callback function: (none) */
-#define  STRETCH_PICTURE_4                20      /* control type: picture, callback function: InsblwCallback */
-#define  STRETCH_NUMERIC                  21      /* control type: numeric, callback function: (none) */
-#define  STRETCH_PICTURE_3                22      /* control type: picture, callback function: DeleteCallback */
+#define  STRETCH_LOADTABLE                2       /* control type: table, callback function: (none) */
+#define  STRETCH_STRAINTABLE              3       /* control type: table, callback function: (none) */
+#define  STRETCH_TEXTMSG_11               4       /* control type: textMsg, callback function: (none) */
+#define  STRETCH_TEXTMSG_12               5       /* control type: textMsg, callback function: (none) */
+#define  STRETCH_TEXTMSG_13               6       /* control type: textMsg, callback function: (none) */
+#define  STRETCH_TEXTMSG_15               7       /* control type: textMsg, callback function: (none) */
+#define  STRETCH_TEXTMSG_10               8       /* control type: textMsg, callback function: (none) */
+#define  STRETCH_TEXTMSG_9                9       /* control type: textMsg, callback function: (none) */
+#define  STRETCH_TEXTMSG_16               10      /* control type: textMsg, callback function: (none) */
+#define  STRETCH_TEXTMSG_8                11      /* control type: textMsg, callback function: (none) */
+#define  STRETCH_TEXTMSG                  12      /* control type: textMsg, callback function: (none) */
+#define  STRETCH_DELETE                   13      /* control type: textMsg, callback function: DeleteCallback */
+#define  STRETCH_INSBLW                   14      /* control type: textMsg, callback function: InsblwCallback */
+#define  STRETCH_PICTURE_5                15      /* control type: picture, callback function: InsabvCallback */
+#define  STRETCH_TEXTMSG_14               16      /* control type: textMsg, callback function: (none) */
+#define  STRETCH_INSABV                   17      /* control type: textMsg, callback function: InsabvCallback */
+#define  STRETCH_NUMERIC_3                18      /* control type: numeric, callback function: (none) */
+#define  STRETCH_NUMERIC_2                19      /* control type: numeric, callback function: (none) */
+#define  STRETCH_PICTURE_6                20      /* control type: picture, callback function: (none) */
+#define  STRETCH_PICTURE_4                21      /* control type: picture, callback function: InsblwCallback */
+#define  STRETCH_NUMERIC                  22      /* control type: numeric, callback function: (none) */
+#define  STRETCH_PICTURE_3                23      /* control type: picture, callback function: DeleteCallback */
+#define  STRETCH_MODE                     24      /* control type: textMsg, callback function: ModeCallback */
 
 
      /* Control Arrays: */
@@ -52,6 +54,7 @@
 int  CVICALLBACK DeleteCallback(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK InsabvCallback(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK InsblwCallback(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK ModeCallback(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 
 
 #ifdef __cplusplus
